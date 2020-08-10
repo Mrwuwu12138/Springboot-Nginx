f051bbb5b852a5eee3076748cf2965173d6c14ce
Mrwuwu12138 <2054672769@qq.com>
2020-08-10 20:55:30 +0800

    update

--- src/main/java/com/springbootstudy/controller/CategoryController.java
+++ src/main/java/com/springbootstudy/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.springbootstudy.controller;
 import com.springbootstudy.Mapper.CategoryMapper;
 import com.springbootstudy.pojo.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,17 @@ public class CategoryController {
     @Autowired
     CategoryMapper categoryMapper;
 
+    @Value("${server.port}")
+    String port;
+
     @RequestMapping("/findAll")
     public List<Category> findAll(){
         List<Category> list = categoryMapper.findAll();
         return list;
+    }
+
+    @RequestMapping("/getPost")
+    public String getPost(){
+        return port;
     }
 }
